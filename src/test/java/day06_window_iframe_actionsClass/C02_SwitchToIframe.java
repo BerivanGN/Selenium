d3428980ce381988598400f5ff5189c47c6d8a12
BerivanGN <berivangnc@gmail.com>
2023-02-21 14:49:51 +0300

    selenium6

---
+++ src/test/java/day06_window_iframe_actionsClass/C02_SwitchToIframe.java
@@ -0,0 +1,66 @@
+package day06_window_iframe_actionsClass;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C02_Iframe extends TestBase {
+    // https://the-internet.herokuapp.com/iframe adresine gidin
+    // "Your content goes here." yazısının görünür olduğunu test edin
+
+    @Test
+    public void test01(){
+
+        // https://the-internet.herokuapp.com/iframe adresine gidin
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+        // "Your content goes here." yazısının görünür olduğunu test edin
+        /*
+           Bazen locate işleminden emin olmamıza rağmen
+           locator çalışmaz
+           Bunun sebebi kullanmak istediğimiz webelementin
+           bir "iframe'in" child'i olması olabilir
+
+           Eğer kullanmak istediğimiz webelement
+           anasayfa içine gömülen başka bir websayfasının elementi ise
+           önce gömülü sayfaya geçmemiz gerekir.
+
+           HTML komutlarında sayfa içerisine sayfa gömebilmek için
+           <iframe> kullanılır
+           Biz de istediğimiz elementi kullanmak için önce
+           o frame'e switch yapmalıyız.
+
+           1- geçiş yapmak istediğiniz frame'i locate edin
+           2- switchTo() ile o frame'e geçiş yapın
+           3- Sonra istediğiniz element ile istediğiniz işlemi yapın
+         */
+        WebElement frameElementi= driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame(frameElementi);
+
+        WebElement yaziElementi= driver.findElement(By.tagName("p"));
+        Assert.assertTrue(yaziElementi.isDisplayed());
+
+        // Elemental Selenium linkine click yapın
+        /*
+           Bir frame'e geçiş yaptıktan sonra
+           oradan çıkıncaya veya başka bir frame'e geçinceye kadar
+           driver o frame içerisinde kalır
+
+           işimiz bittiğinde veya başka bir frame'e geçmek istediğimizde
+           önce çalışacağımız yere geçiş yapmalıyız
+         */
+
+        driver.switchTo().parentFrame(); // iç içe frame'lerde bir üst seviyeye çıkar
+        //driver.switchTo().defaultContent(); // direkt ana sayfaya çıkar
+
+        driver.findElement(By.linkText("Elemental Selenium")).click();
+
+        bekle(3);
+
+
+
+    }
+
+}
---
+++ src/test/java/day06_window_iframe_actionsClass/C03_SwitchToWindows.java
@@ -0,0 +1,75 @@
+package day06_window_iframe_actionsClass;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import utilities.TestBase;
+
+public class C03_SwitchWindows extends TestBase {
+    @Test
+    public void test01(){
+        // amazon anasayfaya gidip arama kutusunun erişilebilir olduğunu test edin
+        driver.get("https://www.amazon.com");
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(aramaKutusu.isEnabled());
+        String amazonWindowHandleDegeri= driver.getWindowHandle();
+        bekle(2);
+
+        // yeni bir tab olarak wisequarter.com'a gidin
+        // url'nin wisequarter içerdiğini test edin
+
+        /* önce yeni bir tab oluşturup
+           driver'ı o sayfaya geçirmeliyiz
+         */
+        driver.switchTo().newWindow(WindowType.WINDOW.TAB);
+        driver.get("https://www.wisequarter.com");
+
+        String expectedIcerik="wisequarter";
+        String actualUrl= driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedIcerik));
+        String wiseWindowHandleDegeri= driver.getWindowHandle();
+
+        // amazon'un açık olduğu tab'a geri dönüp
+        /*
+           Daha önce açılmış olan bir window'a geçiş yapmak için
+           o sayfanın windowHandle değerine ihtiyacımız var
+
+           Testimiz böyle bir görevi içeriyorsa
+           o sayfada iken windowHandle değerini alıp kayıt yapmalıyız
+
+         */
+        driver.switchTo().window(amazonWindowHandleDegeri);
+
+        // Nutella için arama yapın
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        // Sonuçların Nutella içerdiğini test edin
+        String actualSonucYazisi=driver
+                .findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"))
+                .getText();
+        String expectedicerik="Nutella";
+        Assert.assertTrue(actualSonucYazisi.contains(expectedicerik));
+
+        // yeni bir window olarak youtube.com'a gidin
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.youtube.com");
+
+        // url'nin youtube içerdiğini test edin
+        expectedIcerik="youtube";
+        actualUrl= driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedIcerik));
+
+        // wisequarter'ın açık olduğu sayfaya dönün
+        driver.switchTo().window(wiseWindowHandleDegeri);
+
+        // Title'in WiseQuarter içerdiğini test edin.
+        expectedIcerik="Wise Quarter";
+        String actualtitle= driver.getTitle();
+        Assert.assertTrue(actualtitle.contains(expectedIcerik));
+
+        bekle(3);
+    }
+}
